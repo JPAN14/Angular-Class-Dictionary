0430b76924e92f44eea96c06175c492b41cbc5a3
Jay Pan <jay_pan@Jay-Pans-MacBook-Pro.local>
2017-09-26 14:49:25 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,33 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  classList: any;
+  classListKeys: string[];
+  searchText: string;
+  searchResult: string;
+
+  constructor(private http: HttpClient) {
+    // Looks like there is an issue with the HttpClient when parsing JSON
+    // https://github.com/angular/angular/issues/18680#issuecomment-330425866
+    // For now, I added a responseType of 'text'
+    http.get('./assets/class-email-dictionary.json', { responseType: 'text' })
+      .subscribe(data => {
+        this.classList = JSON.parse(data);
+        this.classListKeys = Object.keys(this.classList);
+      }
+    );
+  }
+
+  search() {
+    let email = this.classList[this.searchText];
+    let errorMessage = 'NO MATCH NAME. PLEASE TRY AGAIN';
+
+    if (email) {
+      this.searchResult = email;
+    } else {
+      this.searchResult = errorMessage;
+    }
+  }
 }
